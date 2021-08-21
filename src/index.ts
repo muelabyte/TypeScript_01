ef184d4a6b0828868a12ae9c1ccc36ca038877f3
samuelmv <samuelmelendezv@gmail.com>
2021-08-21 14:35:05 -0400

    comit inicial

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre1: string = 'samuel';
+
+let hp1: number | string = 95;
+let estaVivo1: boolean = true;
+
+nombre1 = nombre1;
+
+console.log(nombre1, hp1);
